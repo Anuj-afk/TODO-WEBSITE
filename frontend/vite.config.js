ff94ca4abeb3358d4c9197cc1bf73ca418e3c83d
Anuj Gupta <65494292+Anuj-afk@users.noreply.github.com>
2024-10-19 11:25:03 +0530

    minor error changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TODO-WEBSITE/"
+  base: "/TODO-WEBSITE/",
   server: {
     port: 3000,
   }
