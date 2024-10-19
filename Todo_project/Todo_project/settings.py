d6b723c86fbccfe3e899a190439815dcaa1dd738
Anuj Gupta <65494292+Anuj-afk@users.noreply.github.com>
2024-10-19 14:27:27 +0530

    host change

--- Todo_project/Todo_project/settings.py
+++ Todo_project/Todo_project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-=m88!n8gbx&4=-ebd74oa-e^7h==yg$_8^s2^&%v9qt+3ogogf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["http://127.0.0.1:8000/", "127.0.0.1", "http://localhost:3000/","https://anuj-afk.github.io/TODO-WEBSITE/","https://anuj-afk.github.io/", ".vercel.app"]
+ALLOWED_HOSTS = ["http://127.0.0.1:8000/", "127.0.0.1", "http://localhost:3000/","https://anuj-afk.github.io/TODO-WEBSITE/","https://anuj-afk.github.io/", ".vercel.app", "*"]
 
 
 # Application definition
