505c14e3fa0ac3b06a1882c27d60bee272f6b17c
Anuj Gupta <65494292+Anuj-afk@users.noreply.github.com>
2024-10-19 11:47:21 +0530

    route changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,21 @@ import 'flatpickr/dist/flatpickr.min.css';
 import 'flatpickr/dist/themes/material_green.css';
 
 import flatpickr from 'flatpickr';
-
-// Font Awesome CSS
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { createBrowserRouter } from 'react-router-dom';
 
-
+const router = createBrowserRouter([
+  {
+    path: '/TODO-WEBSITE/',
+    element: <App></App>
+    children: [
+      { path: '/TODO-WEBSITE/Today', element: <Today></Today> },
+      { path: '/TODO-WEBSITE/Upcoming', element: <Upcoming></Upcoming> },
+      { path: '/TODO-WEBSITE/Completed', element: <Completed></Completed> },
+      { path: '/TODO-WEBSITE/Missed', element: <Missed></Missed> },
+    ]
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
